00acd8b134e1a6a9c640351459cf3b954ea5576f
tommy <forwellzia@gmail.com>
2019-12-04 16:38:26 +0800

    App Init

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+	<el-row>
+		<el-col :span="24" class="bg-orange">
+			<div>123</div>
+		</el-col>
+	</el-row>
+</template>
+
+<script>
+	export default{
+		
+	}
+</script>
+
+<style>
+	.bg-orange{
+		background-color: orange;
+	}
+</style>
