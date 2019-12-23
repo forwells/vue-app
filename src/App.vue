8c7f535900ad7af68b1cc2b602c3190e3772b437
tommy <forwellzia@gmail.com>
2019-12-23 09:06:17 +0800

    home update

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="app">
+  <div id="appclication">
     <Header></Header>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue'
 export default {
-  name: 'app',
+  name: 'application',
   components: {
     Header
   }
@@ -16,12 +15,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,68 @@
 <template>
-	<el-row>
-		<el-col :span="20" class="col-bg">
-			<div>123</div>
-		</el-col>
-	</el-row>
+	<el-header class="bg-header">
+		<el-row>
+			<el-col :span="6">
+				<img :src="logo.src" :alt="logo.alt" class="logo">
+			</el-col>
+			<el-col :span="18" class="menu-container">
+				<el-menu :default-active="0" class="hoa-menu" mode="horizontal">
+					<el-menu-item v-for="navItem in menu" :key="navItem.sort" :index="navItem.sort">
+						<a :href="navItem.link">{{navItem.title}}</a>
+					</el-menu-item>
+				</el-menu>
+			</el-col>
+		</el-row>
+	</el-header>
 </template>
 
 <script>
-	export default{
+	export default {
+		data: function() {
+			return {
+				logo: {
+					alt: "Hoalook",
+					src: "/upload/images/hoalook-font-logo.png"
+				},
+				menu: {
+					0: {
+						title: "Magento",
+						link: "#",
+						sort: 0,
+
+					},
+					1: {
+						title: "Shopify",
+						link: "",
+						sort: 1,
+					}
+				}
+			}
+		},
 		
+		methods: {
+			// handleSelect(key, keyPath) {
+			// 	console.log(key, keyPath);
+			// }
+		}
 	}
 </script>
 
 <style lang="stylus" scoped>
-	.col-bg
-		background-color:#eee;
-		box-shadow 0 0 0 5 #ccc;
+	.bg-header 
+		background-color:#F2F6FC;
+		box-shadow 0 0 5px 5px #ccc;
+	.logo 
+		margin-left:50%;
+	.menu-container
+		padding 0 50px 0 50px;
+	.hoa-menu
+		
+		background-color transparent;
+	.el-menu.el-menu--horizontal
+		border none;
+	.el-menu--horizontal>.el-menu-item.is-active
+		border-bottom-color #E6A23C;
+	.el-menu--horizontal>.el-menu-item a{
+		text-decoration none;
+	}
 </style>
