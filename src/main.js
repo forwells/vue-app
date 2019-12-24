9269f78de07d16778c93a356f79e9bd7005f486e
tommy <forwelll@163.com>
2019-12-24 18:36:22 +0800

    vue router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
-Vue.config.productionTip = false
-
+// Vue.config.productionTip = false
+Vue.use(VueRouter)
 Vue.use(ElementUI)
 new Vue({
   render: h => h(App),
